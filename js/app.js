e956910f97dfb574c11baade2c3b91ebf0994571
trev4ev <trevoraquino@gmail.com>
2015-04-12 22:25:15 -0700

    add simpler buttons

--- js/app.js
+++ js/app.js
@@ -1,27 +1,40 @@
 $(document).ready(function(){
     var pattern = Trianglify({
-                height: window.innerHeight * 3,
+                height: window.innerHeight,
                 width: window.innerWidth,
                 cell_size: 100,
                 x_colors: 'Purples'
             });
             document.body.appendChild(pattern.canvas());
-    
-    $('#spacer').css("margin-top", window.innerHeight);
+ 
+    $('.button').css("left", (window.innerWidth/2 - 23));
+    $('.button2').css("left", (window.innerWidth/2 - 23));
+    $('.button').css("top", (window.innerHeight - 60));
+    $('.button2').css("top", (window.innerHeight * 2 - 60));
+    $('#about').css("height", window.innerHeight);
+    $('#about').css("margin-top", (window.innerHeight - 375)/3);
     $('h1').hide();
     $('.button').hide();
+    $('.button2').hide();
     $('h1').delay(1000).fadeIn(1000);
     $('.button').delay(1400).fadeIn(700);
+    $('.button2').delay(1400).fadeIn(700);
 });
 
 $('#scroll').click(function(){
     $('html, body').animate({
-    scrollTop: $("#spacer").offset().top
+    scrollTop: $("#about").offset().top
 }, 1000);
 });
 
 $('.button').hover(function(){
-    $('.button').animate({backgroundColor:'white', color: '#806bc9'});
+    $('.button').animate({backgroundColor:'white'});
 }, function(){
-    $('.button').animate({backgroundColor: 'rgba(0, 0, 0, 0)' , color: 'white'});
+    $('.button').animate({backgroundColor: 'rgba(0, 0, 0, 0)'});
+});
+
+$('.button2').hover(function(){
+    $('.button2').animate({backgroundColor:'#806bc9'});
+}, function(){
+    $('.button2').animate({backgroundColor: 'rgba(0, 0, 0, 0)'});
 });
