348c2476bdf69c40d866dad86d29e49c06f55b3a
Trevor <trevoraquino@gmail.com>
2016-02-25 19:55:06 -0800

    add random animation

--- js/app.js
+++ js/app.js
@@ -11,11 +11,12 @@ setInterval(function(){
     var e = document.createElement('div');
     e.setAttribute('class', 'circle');
     e.setAttribute('id', 'temp');
-    e.style.left = x - 5 + 'px';
-    e.style.top = y - 5 + 'px';
+    e.style.left = Math.floor(Math.random() * window.innerWidth) + 'px';
+    e.style.top = Math.floor(Math.random() * window.innerHeight) + 'px';
     document.getElementById('body').appendChild(e);
     leftValue =  Math.floor(Math.random() * window.innerWidth) + "px";
     topValue = Math.floor(Math.random() * window.innerHeight) + "px";
+    $('#temp').animate({opacity: '0.4'}, 200);
     $('#temp').animate({left: leftValue, top: topValue}, Math.floor((Math.random() * 5)+5)*1000);
     $('#temp').animate({opacity: '0'}, 1000);
     document.getElementById('temp').setAttribute('id', "done");
